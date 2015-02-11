36899025499c417c1fd24e6f5b7303359e95ec51
Mark Lee <marklee.lhs@gmail.com>
2015-02-10 21:41:53 -0800

    Update npm folder and sync version with lodash

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "erasaur:lodash",
+  name: "erasaur:meteor-lodash",
   summary: "Wrapper for Lo-Dash v3.1.0",
   version: "3.1.0",
   git: "https://github.com/erasaur/meteor-lodash.git"
