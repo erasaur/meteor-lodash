de4c94e0ce5796daeae1946309477f403e5b8eb1
Mark Lee <marklee.lhs@gmail.com>
2015-05-05 20:06:15 -0700

    Bump to v3.8.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "erasaur:meteor-lodash",
-  summary: "Wrapper for Lo-Dash v3.7.0",
-  version: "3.7.0",
+  summary: "Wrapper for Lo-Dash v3.8.0",
+  version: "3.8.0",
   git: "https://github.com/erasaur/meteor-lodash.git"
 });
 
